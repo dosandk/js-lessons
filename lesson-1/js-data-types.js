eac7a89668da860186f0acd69b4fc9d306ea100b
Vladimir Shevchuk <dosandkv@gmail.com>
2016-01-23 18:44:09 +0200

    add lesson-1;

---
+++ lesson-1/js-data-types.js
@@ -0,0 +1,56 @@
+console.log('ЛИТЕРАЛЫ');
+ console.log('простые типы данных');
+
+var myString = 'my-string';
+
+var myBoolean = false;
+var myNull = null;
+var myUndefined = undefined;
+
+// сложные типы данных (объектные)
+var myObj = {};
+var myArr = [];
+var myFunction = function() {};
+
+// ========================================
+
+var myNubmer = 20;
+    //console.log('myNubmer', myNubmer + 1);
+
+var myNubmerConstructor = new Number(10);
+    //console.log('myNubmerConstructor', myNubmerConstructor + 1);
+
+
+// КОНСТРУКТОРЫ
+
+// простые типы данных
+    //console.log('myNubmerConstructor', myNubmerConstructor);
+    //console.log('typeof ', typeof myNubmerConstructor);
+
+var myStringConstructor = new String('myString');
+    //console.log('myStringConstructor', myStringConstructor);
+    //console.log('typeof', typeof myStringConstructor);
+
+var myBooleanConstructor  = new Boolean();
+    //console.log('myBooleanConstructor', myBooleanConstructor);
+    //console.log('typeof', typeof myBooleanConstructor);
+
+var myNullConstructor = null;
+    //console.log('myNullConstructor', myNullConstructor);
+    //console.log('typeof', typeof myNullConstructor);
+
+var myUndefinedConstructor = undefined;
+    //console.log('myUndefinedConstructor', myUndefinedConstructor);
+    //console.log('typeof', typeof myUndefinedConstructor);
+
+// сложные типы данных (объектные)
+
+var myObjConstructor = new Object();
+    //console.log('myObjConstructor', myObjConstructor);
+    //console.log('typeof', typeof myObjConstructor);
+var myArrConstructor = new Array();
+    //console.log('myArrConstructor', myArrConstructor);
+    //console.log('typeof', typeof myArrConstructor);
+var myFunctionConstructor = new Function();
+    //console.log('myFunctionConstructor', myFunctionConstructor);
+    //console.log('typeof', typeof myFunctionConstructor);
---
+++ lesson-1/strings.js
@@ -0,0 +1,11 @@
+ var mySuperString = "hello, this is my super string";
+
+ //console.log(mySuperString.charAt(0));
+ //console.log(mySuperString.substring(5, 12));  // начало, конец
+ //console.log(mySuperString.substr(5, 12)); // начало, кол-во символов
+ //console.log(mySuperString.indexOf('івал'));
+ //mySuperString.lastIndexOf('is'); // начало, кол-во символов
+ //mySuperString.replace('is', 'is not'); // начало, кол-во символов
+ //mySuperString.split(''); // начало, кол-во символов
+ //console.log(mySuperString.split(''));
+ //console.log(mySuperString.toLowerCase());
