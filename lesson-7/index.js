786bb260768ae45bc4739af284375472cdf32332
Vladimir Shevchuk <dosandkv@gmail.com>
2016-03-07 10:21:28 +0200

    added object-part-2 homework;

---
+++ lesson-7/index.js
@@ -0,0 +1,21 @@
+// null/undefined/boolean
+
+//console.log('a' < 'b');
+//console.log(5 < 6);
+
+//NaN
+//''
+//null
+//undefined
+//0
+//=====
+//false
+
+var myString = '';
+
+if (!myString) {
+    console.log(false);
+}
+
+console.log(Boolean(myString));
+console.log(!!myString);
---
+++ shedule.md
@@ -0,0 +1,5 @@
+* Sat Feb 27 2016 19:53:42 GMT+0200 (EET)
+Наследование в js
+
+* Wed Mar 02 2016 20:26:14 GMT+0200 (EET)
+null/undefined/boolean
